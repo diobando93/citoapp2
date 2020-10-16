916896f8ae75878efc4ef2d02503fbc9105ea5a1
diobando93 <devisrael93@gmail.com>
2020-10-15 19:51:46 -0500

    pdf generado v1

--- src/process/resultado_process.js
+++ src/process/resultado_process.js
@@ -1,112 +1,145 @@
-const {ipcMain} = require('electron');
+const { ipcMain } = require("electron");
 
-const PDFDocument = require('pdfkit');
-const Patient = require('../models/patient.js');
-const Pedido = require('../models/pedido.js');
-const Results = require('../models/results.js');
+const PDFDocument = require("pdfkit");
+const Patient = require("../models/patient.js");
+const Pedido = require("../models/pedido.js");
+const Results = require("../models/results.js");
 
-var fs = require('fs');
-var resultPdf
-let nombre = '';
-let edad = '';
-let pedido = '';
+var fs = require("fs");
+var resultPdf;
+let nombre = "";
+let edad = "";
+let pedido = "";
 
+function consultar() {
+  ipcMain.on("consulta", async (e, args) => {
+    console.log(args);
+    const paciente = await Patient.find({ cedula: args[0] });
+    const pedido = await Pedido.find({ cedula: args[0], pedido: args[1] });
+    e.reply("pacienteRetrieved", JSON.stringify(paciente));
+    console.log(paciente);
 
-function consultar(){
-    ipcMain.on('consulta', async(e, args) =>{
-        console.log(args);
-        const paciente = await Patient.find({cedula: args[0]});
-        const pedido = await Pedido.find({cedula: args[0], pedido: args[1]});
-        e.reply('pedidoRetrieved', JSON.stringify(pedido));
-        console.log(pedido);
-        e.reply('pacienteRetrieved', JSON.stringify(paciente));
-        console.log(paciente);
-        //nombre = nombre.concat(paciente.nombres);
-        //console.log(nombre);
-        //nombre = 'Nombre: ' //+ paciente.nombres;
-        //edad = 'Edad: ' //+ pedido.edad;
-        //pedido = 'Pedido: ' //+ pedido.pedido;
-        //console.log(pedido);
-        /*
+    e.reply("pedidoRetrieved", JSON.stringify(pedido));
+    console.log(pedido);
+  });
+  ipcMain.on("datosResultado", async (e, args) => {
+    //console.log(args);
+    const resultadoBD = {
+      h_clinica: args[28],
+      cedula: args[0],
+      pedido: args[1],
+      aspecto_cuello: args[2],
+      observaciones: args[3],
+      responsable: args[4],
+      noplacas: args[5],
+      madecuada: args[6],
+      minadecuada: args[7],
+      minforme: args[8],
+      frotis: args[9],
+      fnoprocesada: args[10],
+      germenes: {
+        floraBarcilar: args[11],
+        floraCocoide: args[12],
+        vaginosisBacteriana: args[13],
+        triconomas: args[14],
+        actnomyces: args[15],
+        candida: args[16],
+        herpes: args[17],
+        hpv: args[18],
+        otros: args[19],
+      },
+      celulas: {
+        endocervicales: args[20],
+        metaplasticas: args[21],
+        endometriales: args[22],
+      },
+      result_toma: {
+        diagnostico: args[23],
+        recomendacion: args[24],
+        observaciones: args[25],
+        citotecnologo: args[26],
+        citopatologo: args[27],
+      },
+    };
+    resultPdf = args;
+    //console.log(resultadoBD);
 
-        */
-    })
-    ipcMain.on('datosResultado', async (e, args) => {
-        //console.log(args);
-        const resultadoBD = {
-            h_clinica: args[28],
-            cedula: args[0],
-            pedido: args[1],
-            aspecto_cuello: args[2],
-            observaciones: args[3],
-            responsable: args[4],
-            noplacas: args[5],
-            madecuada: args[6],
-            minadecuada: args[7],
-            minforme: args[8],
-            frotis: args[9],
-            fnoprocesada: args[10],
-            germenes: {
-                floraBarcilar: args[11],
-                floraCocoide: args[12],
-                vaginosisBacteriana: args[13],
-                triconomas: args[14],
-                actnomyces: args[15],
-                candida: args[16],
-                herpes: args[17],
-                hpv: args[18],
-                otros: args[19],
-            },
-            celulas: {
-                endocervicales: args[20],
-                metaplasticas: args[21],
-                endometriales: args[22],
-            },
-            result_toma: {
-                diagnostico: args[23],
-                recomendacion: args[24],
-                observaciones: args[25],
-                citotecnologo: args[26],
-                citopatologo: args[27],
-            }
-
-        }
-        resultPdf = resultadoBD;
-        //console.log(resultadoBD);
-
-        //validar formulario
-        //guardar en bd
-        const newResult = new Results(resultadoBD);
-        const resultadoSaved = await newResult.save();
-        genPDF();
-        // generar pdf
-
-    });
+    //validar formulario
+    //guardar en bd
+    const newResult = new Results(resultadoBD);
+    const resultadoSaved = await newResult.save();
+    genPDF();
+    // generar pdf
+  });
 }
 
-function genPDF(){
-    console.log(resultPdf);
-    console.log(nombre);
-    //console.log(edad);
-    //console.log(pedido);
+function genPDF() {
+  if (resultPdf[6] == true) {
+    muestraPDF = "xxx  Adecuada";
+    muestra1PDF = "    Inadecuada";
+  } else {
+    muestraPDF = "Adecuada   ";
+    muestra1PDF = "xxx Inadecuada";
+  }
 
-    //texto  = texto.concat(paciente.nombres);
-    /*
-    const doc = new PDFDocument;
-    doc.pipe(fs.createWriteStream('output.pdf'));
-    doc.text(texto, {
-        columns: 3,
-        columnGap: 15,
-        height: 100,
-        width: 465,
-        align: 'justify'
-    });
-    doc.end();
-    */
+  if (resultPdf[7] == true) {
+    muestraPDF = "xxx  Inadecuada";
+    muestra1PDF = "    Adecuada   ";
+  }
+  if (resultPdf[11] == true) {
+    floraBarcilarPDF = "xxx  Flora Bacilar";
+  } else {
+    floraBarcilarPDF = "Flora Bacilar";
+  }
+  if (resultPdf[12] == true) {
+    floraCocoidePDF = "xxx  Flora Cocoide";
+  } else {
+    floraCocoidePDF = "Flora Cocoide";
+  }
+  if (resultPdf[13] == true) {
+    vaginosisBacterianaPDF = "xxx  Vaginosis Bacteriana";
+  } else {
+    vaginosisBacterianaPDF = "Vaginosis Bacteriana";
+  }
+  if (resultPdf[14] == true) {
+    triconomasPDF = "xxx  Triconomas";
+  } else {
+    triconomasPDF = "Triconomas";
+  }
+
+  const doc = new PDFDocument();
+  doc.pipe(fs.createWriteStream("output.pdf"));
+  doc
+    .font("Times-Roman", 13)
+    .moveDown()
+    .text("Nombre Completo: " + resultPdf[29], 100, 100)
+    .text("Edad: " + resultPdf[30], 400, 100)
+    .moveDown()
+    .text("F.U.M: Hace 10 años " + resultPdf[31], 100, 130)
+    .text("g: " + resultPdf[33] + "p: " + resultPdf[34], 400, 130)
+    .moveDown()
+    .text("Fecha toma: " + resultPdf[32], 100, 160)
+    .moveDown()
+    .text("Solicitado por: ")
+    .moveDown()
+    .text("VALORACION DE LA MUESTRA")
+    .moveDown()
+    .text("La muestra es: " + muestraPDF, 100, 230)
+    .text(muestra1PDF, 300, 230)
+    .moveDown()
+    .text("Frotis: " + resultPdf[9], 100, 250)
+    .moveDown()
+    .text("Gérmenes")
+    .moveDown()
+    .text(floraBarcilarPDF, 50, 290)
+    .text(floraCocoidePDF, 150, 290)
+    .text(vaginosisBacterianaPDF, 250, 290)
+    .text(triconomasPDF, 400, 290);
+
+  doc.end();
 }
 
-
-module.exports ={consultar}
+module.exports = { consultar };
 /*
 ipcMain.on('get-task', async (e, args) => {
     const tasks = await Task.find();
--- src/ui/scripts/resultados_render.js
+++ src/ui/scripts/resultados_render.js
@@ -1,5 +1,5 @@
-const electron = require('electron');
-const {ipcRenderer} = electron;
+const electron = require("electron");
+const { ipcRenderer } = electron;
 const cedula = document.getElementById("cedula");
 const pedido = document.getElementById("pedido");
 let cedula1 = document.getElementById("cedula1");
@@ -48,64 +48,78 @@ const citotecnologo = document.getElementById("citotecnologo");
 
 let datosResultado = [];
 
-function consulta_render(){
-    ipcRenderer.on('response', (e, args) =>{
-        const datos = JSON.parse(args);
-        console.log(datos);
-    })
+function consulta_render() {
+  ipcRenderer.on("response", (e, args) => {
+    const datos = JSON.parse(args);
+    console.log(datos);
+  });
 }
 
-function retrive(){
-    console.log(cedula.value);
-    console.log(pedido.value);
-    datosResultado.push(cedula.value);
-    datosResultado.push(pedido.value);
-    ipcRenderer.send('consulta', datosResultado);
-    ipcRenderer.on('pedidoRetrieved', (e, args) => {
-        const pedidoRetrieved = JSON.parse(args);
-        console.log(pedidoRetrieved);
-        pedido1.innerHTML = pedidoRetrieved[0].pedido; 
-        //datosResultado.push(args[])
-    })
-    ipcRenderer.on('pacienteRetrieved', (e, args) => {
-        const pacienteRetrieved =JSON.parse(args);
-        console.log(pacienteRetrieved);
-        hclinica = pacienteRetrieved[0].h_clinica;
-        //console.log(pacienteRetrieved[0].nombres);
-        nombre.innerHTML = pacienteRetrieved[0].nombres + " " + pacienteRetrieved[0].apellidos
-        document.getElementById("datosBusqueda").style.display = "none";
-    })
+function retrive() {
+  console.log(cedula.value);
+  console.log(pedido.value);
+  datosResultado.push(cedula.value);
+  datosResultado.push(pedido.value);
+  ipcRenderer.send("consulta", datosResultado);
+  ipcRenderer.on("pedidoRetrieved", (e, args) => {
+    const pedidoRetrieved = JSON.parse(args);
+    console.log(pedidoRetrieved);
+    edadPDF = pedidoRetrieved[0].edad;
+    fumPDF = pedidoRetrieved[0].fecha_ult_mestruacion;
+    fmuestraPDF = pedidoRetrieved[0].f_muestra;
+    partosPDF = pedidoRetrieved[0].num_partos;
+    abortosPDF = pedidoRetrieved[0].num_abortos;
+    //pedido1.innerHTML = pedidoRetrieved[0].pedido;
+    //datosResultado.push(args[])
+  });
+  ipcRenderer.on("pacienteRetrieved", (e, args) => {
+    const pacienteRetrieved = JSON.parse(args);
+    console.log(pacienteRetrieved);
+    nombrePDF =
+      pacienteRetrieved[0].nombres + " " + pacienteRetrieved[0].apellidos;
+    hclinicaPDF = pacienteRetrieved[0].h_clinica;
 
+    //console.log(pacienteRetrieved[0].nombres);
+    nombre.innerHTML =
+      pacienteRetrieved[0].nombres + " " + pacienteRetrieved[0].apellidos;
+    document.getElementById("datosBusqueda").style.display = "none";
+  });
 }
 
-function enviaDatos(){
-    datosResultado.push(acuello.value);
-    datosResultado.push(observaciones.value);
-    datosResultado.push(responsable.value);
-    datosResultado.push(noplacas.value);
-    datosResultado.push(madecuada.checked);
-    datosResultado.push(minadecuada.checked);
-    datosResultado.push(noinformar.value);
-    datosResultado.push(frotis.value);
-    datosResultado.push(fnoprocesada.value);
-    datosResultado.push(fbacilar.checked);
-    datosResultado.push(fcocoide.checked);
-    datosResultado.push(vbacteriana.checked);
-    datosResultado.push(triconomas.checked);
-    datosResultado.push(actnomyces.checked);
-    datosResultado.push(candida.checked);
-    datosResultado.push(herpes.checked);
-    datosResultado.push(hpv.checked);
-    datosResultado.push(germenesotros.checked);
-    datosResultado.push(endocervicales.value);
-    datosResultado.push(metaplasticas.value);
-    datosResultado.push(endometriales.value);
-    datosResultado.push(diagnostico.value);
-    datosResultado.push(recomendacion.value);
-    datosResultado.push(reobservaciones.value);
-    datosResultado.push(citopatologo.checked);
-    datosResultado.push(citotecnologo.checked);
-    datosResultado.push(hclinica);
-    console.log(datosResultado);
-    ipcRenderer.send('datosResultado', datosResultado);
+function enviaDatos() {
+  datosResultado.push(acuello.value);
+  datosResultado.push(observaciones.value);
+  datosResultado.push(responsable.value);
+  datosResultado.push(noplacas.value);
+  datosResultado.push(madecuada.checked);
+  datosResultado.push(minadecuada.checked);
+  datosResultado.push(noinformar.value);
+  datosResultado.push(frotis.value);
+  datosResultado.push(fnoprocesada.value);
+  datosResultado.push(fbacilar.checked);
+  datosResultado.push(fcocoide.checked);
+  datosResultado.push(vbacteriana.checked);
+  datosResultado.push(triconomas.checked);
+  datosResultado.push(actnomyces.checked);
+  datosResultado.push(candida.checked);
+  datosResultado.push(herpes.checked);
+  datosResultado.push(hpv.checked);
+  datosResultado.push(germenesotros.checked);
+  datosResultado.push(endocervicales.value);
+  datosResultado.push(metaplasticas.value);
+  datosResultado.push(endometriales.value);
+  datosResultado.push(diagnostico.value);
+  datosResultado.push(recomendacion.value);
+  datosResultado.push(reobservaciones.value);
+  datosResultado.push(citopatologo.checked);
+  datosResultado.push(citotecnologo.checked);
+  datosResultado.push(hclinicaPDF);
+  datosResultado.push(nombrePDF);
+  datosResultado.push(edadPDF);
+  datosResultado.push(fumPDF);
+  datosResultado.push(fmuestraPDF);
+  datosResultado.push(partosPDF);
+  datosResultado.push(abortosPDF);
+  console.log(datosResultado);
+  ipcRenderer.send("datosResultado", datosResultado);
 }
